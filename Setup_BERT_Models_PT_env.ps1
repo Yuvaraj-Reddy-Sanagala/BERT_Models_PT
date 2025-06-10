41dd86960268530c4fc2bc953a2e3d890bae70fe
Yuvaraj Reddy Sanagala <yuvarajs0920@gmail.com>
2025-06-10 00:09:47 -0400

    Pushing code files

--- Setup_BERT_Models_PT_env.ps1
+++ Setup_BERT_Models_PT_env.ps1
@@ -6,7 +6,7 @@ python -m venv BERT_Models_PT_env
 
 # Install required packages
 pip install ipykernel
-pip install torch transformers datasets scikit-learn seqeval pandas matplotlib seaborn flask
+pip install torch transformers datasets accelerate scikit-learn seqeval pandas matplotlib seaborn flask
 
 # Register kernel for Jupyter
 python -m ipykernel install --user --name=bert-pt --display-name "BERT_Models_PT"
